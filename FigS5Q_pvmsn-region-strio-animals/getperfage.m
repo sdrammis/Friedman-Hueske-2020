1e5483ebc0888d9fbd7e5148f6ab7b71f3438aad
sdrammis <sdrammis@mit.edu>
2019-10-08 11:32:03 -0400

    Rename FigS5Q folder name, remove strio

--- FigS5Q_pvmsn-region-strio-animals/getperfage.m
+++
@@ -1,9 +0,0 @@
-function age = getperfage(mouse)
-birthDate = datetime(mouse.birthDate, 'InputFormat', 'yyyy-MM-dd');
-try
-    perfDate = datetime(mouse.perfusionDate, 'Inputformat', 'yy/MM/dd');
-catch
-    perfDate = datetime(mouse.perfusionDate, 'Inputformat', 'yyyy-MM-dd');
-end
-age = calmonths(caldiff([birthDate, perfDate], 'months'));
-end
