5682de6f2b5bbb82534a7f9f11521a1118ee7332
Sebastian <sebatoro@mit.edu>
2019-11-24 16:30:33 -0400

    Figure 4 New

--- Fig4L/correlationsPerGroupBars.m
+++ Fig4L/correlationsPerGroupBars.m
@@ -2,20 +2,25 @@ function correlationsPerGroupBars(twdb,striosomality,engagement,normalized,xType
 
 miceIDs = get_mouse_ids(twdb,0,'WT','all',striosomality,'all','all',1,{});
 [miceTrials,miceFluorTrials,rewardTones,costTones] = get_all_trials(twdb,miceIDs,0,0);
-[WT_reward,WT_cost] = correlationsPerGroup(twdb,miceTrials,miceFluorTrials,rewardTones,costTones,miceIDs,'','',engagement,'p-value',normalized,xType);
-WT_sig_reward =sum(WT_reward < 0.2);
-WT_sig_cost = sum(WT_cost < 0.2);
+[WT_reward,WT_cost,WT_area] = correlationsPerGroup(twdb,miceTrials,miceFluorTrials,rewardTones,costTones,miceIDs,'','',engagement,'R',normalized,xType);
+WT_sig = sum(abs([WT_reward,WT_cost,WT_area]) > 0.7,2) > 0;
+
+WT_strong_reward = sum(abs(WT_reward(WT_sig))>0.7);
+WT_not_strong_reward = sum(abs(WT_reward(WT_sig))<=0.7);
 
 miceIDs = get_mouse_ids(twdb,0,'HD','all',striosomality,'all','all',1,{});
 [miceTrials,miceFluorTrials,rewardTones,costTones] = get_all_trials(twdb,miceIDs,0,0);
-[HD_reward,HD_cost] = correlationsPerGroup(twdb,miceTrials,miceFluorTrials,rewardTones,costTones,miceIDs,'','',engagement,'p-value',normalized,xType);
-HD_sig_reward = sum(HD_reward < 0.2);
-HD_sig_cost = sum(HD_cost < 0.2);
+[HD_reward,HD_cost,HD_area] = correlationsPerGroup(twdb,miceTrials,miceFluorTrials,rewardTones,costTones,miceIDs,'','',engagement,'R',normalized,xType);
+HD_sig = sum(abs([HD_reward,HD_cost,HD_area]) > 0.7,2) > 0;
+
+
+HD_strong_reward = sum(abs(HD_reward(HD_sig))>0.7);
+HD_not_strong_reward = sum(abs(HD_reward(HD_sig))<=0.7);
 
 figure
-bar([WT_sig_reward,WT_sig_cost;HD_sig_reward,HD_sig_cost])
-legend('Reward','Cost')
-ylabel('# mice with sig correlations')
+bar([WT_strong_reward,WT_not_strong_reward;HD_strong_reward,HD_not_strong_reward])
+legend('Strong','Not Strong')
+ylabel('# of d-prime striosomal reward correlations')
 set(gca, 'xtick', 1:2, 'xticklabel', {'WT','HD'});
 
 end
--- Fig4M/correlationsPerGroupBars.m
+++ Fig4M/correlationsPerGroupBars.m
@@ -5,22 +5,22 @@ miceIDs = get_mouse_ids(twdb,0,'WT','all',striosomality,'all','all',1,{});
 [WT_reward,WT_cost,WT_area] = correlationsPerGroup(twdb,miceTrials,miceFluorTrials,rewardTones,costTones,miceIDs,'','',engagement,'R',normalized,xType);
 WT_sig = sum(abs([WT_reward,WT_cost,WT_area]) > 0.7,2) > 0;
 
-WT_neg = sum(WT_area(WT_sig)<0);
-WT_pos = sum(WT_area(WT_sig)>0);
-
 miceIDs = get_mouse_ids(twdb,0,'HD','all',striosomality,'all','all',1,{});
 [miceTrials,miceFluorTrials,rewardTones,costTones] = get_all_trials(twdb,miceIDs,0,0);
 [HD_reward,HD_cost,HD_area] = correlationsPerGroup(twdb,miceTrials,miceFluorTrials,rewardTones,costTones,miceIDs,'','',engagement,'R',normalized,xType);
 HD_sig = sum(abs([HD_reward,HD_cost,HD_area]) > 0.7,2) > 0;
 
 
-HD_neg = sum(HD_area(HD_sig)<0);
-HD_pos = sum(HD_area(HD_sig)>0);
+WT_strong_cost = sum(abs(WT_cost(WT_sig))>0.7);
+WT_not_strong_cost = sum(abs(WT_cost(WT_sig))<=0.7);
+
+HD_strong_cost = sum(abs(HD_cost(HD_sig))>0.7);
+HD_not_strong_cost = sum(abs(HD_cost(HD_sig))<=0.7);
 
 figure
-bar([WT_pos,WT_neg;HD_pos,HD_neg])
-legend('Positive','Negative')
-ylabel('# of d-prime striosomal area correlations')
+bar([WT_strong_cost,WT_not_strong_cost;HD_strong_cost,HD_not_strong_cost])
+legend('Strong','Not Strong')
+ylabel('# of d-prime striosomal cost correlations')
 set(gca, 'xtick', 1:2, 'xticklabel', {'WT','HD'});
 
 end
