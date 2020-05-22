bc46dd310b986b7f6b4d7eb241173de107781307
Jiajia Zhao <snowbabyjia@gmail.com>
2020-05-22 15:49:53 -0400

    fix Fig7CD and add Fig 7E-snr

--- Fig7D/y_mean.m
+++ Fig7D/y_mean.m
@@ -1,38 +1,10 @@
-function [xnew, ynew] = y_mean(x,y, isEntropy, isVariance)
+function [xnew, ynew] = y_mean(x,y)
 xnew = [];
 ynew = [];
-mthre = 0.05;
-uniqx = unique(x);
-prev_ymean = mean(y(x == uniqx(1)));
-for idx=1:length(uniqx)
-    ix = uniqx(idx);
-    ysforx = y(ix == x);
-    m = mean(ysforx);
-%     if length(ysforx) > 3 && m > mthre
-% %     ysforx = ysforx(1,ysforx(1,:)>0);
-%         ysforx = ysforx - m;
-%         s = std(ysforx);
-%         bins = 1;
-%       
-%         if isEntropy
-%             ent = getEntropy(ysforx,bins);
-%             if ent > 0
-%                 xnew = [xnew; ix];
-%                 ynew = [ynew; (prev_ymean - mean(ysforx))/ent];
-%                 prev_ymean = mean(ysforx);
-%             end
-%         elseif isVariance
-%             if s > 0
-%                 xnew = [xnew; ix];
-% %                 ynew = [ynew; (prev_ymean - mean(ysforx))/std(ysforx)];
-% %                 prev_ymean = mean(ysforx);
-%                 ynew = [ynew; 1/s];
-%             end
-%         end
-%     else
-        xnew = [xnew; ix];
-        ynew = [ynew; m];
-%     end
-end
-end
 
+uniqx = unique(x);
+for ix=uniqx
+   xnew = [xnew; ix];
+   ynew = [ynew; mean(y(ix == x))];
+end
+end
