61a78a39cb51872bff7c85f04217943a7dbb3191
Sabrina Drammis <drammis.sabrina@gmail.com>
2020-05-28 15:57:33 -0400

    Update FigS2P to get mice ages

--- FigS2P_transients-snr/runme.m
+++ FigS2P_transients-snr/runme.m
@@ -7,22 +7,23 @@ PERIOD = 'response';
 [groups_matrix, names] = groupmice(miceType, 'Matrix');
 [groups_strio, ~] = groupmice(miceType, 'Strio');
 
-snrs_matrix = get_groups_spikes(twdb, groups_matrix, PERIOD, ENGAGED, USE_FIRST_TASK);
-snrs_strio = get_groups_spikes(twdb, groups_strio, PERIOD, ENGAGED, USE_FIRST_TASK);
+ret_matrix = get_groups_spikes(twdb, groups_matrix, PERIOD, ENGAGED, USE_FIRST_TASK);
+ret_strio = get_groups_spikes(twdb, groups_strio, PERIOD, ENGAGED, USE_FIRST_TASK);
+snrs_matrix = ret_matrix{1}{1};
+mice_matrix = ret_matrix{1}{2};
+snrs_strio = ret_strio{1}{1};
+mice_strio = ret_strio{1}{2};
 
-snrs_matrix_WTL = snrs_matrix{1};
-snrs_strio_WTL = snrs_strio{1};
-
-diffs_matrix = snrs_matrix_WTL(:,2) - snrs_matrix_WTL(:,1);
-diffs_strio = snrs_strio_WTL(:,2) - snrs_strio_WTL(:,1);
+diffs_matrix = snrs_matrix(:,2) - snrs_matrix(:,1);
+diffs_strio = snrs_strio(:,2) - snrs_strio(:,1);
 
 figure;
 subplot(1,2,1);
-[nStrio,datStrio] = plot_group(snrs_strio_WTL);
+[nStrio,datStrio] = plot_group(snrs_strio, mice_strio);
 subplot(1,2,2);
-[nMatrix,datMatrix] = plot_group(snrs_matrix_WTL);
+[nMatrix,datMatrix] = plot_group(snrs_matrix, mice_matrix);
 
-fprintf('======== strio stats =========');
+fprintf('======== strio stats ========= \n');
 x = datStrio(:,1);
 y = datStrio(:,2);
 [~,ttestp] = ttest(x,y);
@@ -31,7 +32,7 @@ fprintf('ttest p = %d \n', ttestp);
 fprintf('signrank p = %d \n', signrankp);
 fprintf('obsersvation = mouse (# = %d) \n', nStrio);
 
-fprintf('======== matrix stats =========');
+fprintf('======== matrix stats ========= \n');
 x = datMatrix(:,1);
 y = datMatrix(:,2);
 [~,ttestp] = ttest(x,y);
@@ -40,9 +41,10 @@ fprintf('ttest p = %d \n', ttestp);
 fprintf('signrank p = %d \n', signrankp);
 fprintf('obsersvation = mouse (# = %d) \n', nMatrix);
 
-function [n, spikes] = plot_group(dat)
+function [n, spikes] = plot_group(dat,mice_)
 hold on;
-spikes = dat(~(isnan(dat(:,1)) | isnan(dat(:,2)) | isinf(dat(:,1)) | isinf(dat(:,2))), :);
+idxs = ~(isnan(dat(:,1)) | isnan(dat(:,2)) | isinf(dat(:,1)) | isinf(dat(:,2)));
+spikes = dat(idxs,:);
 n = size(spikes,1);
 
 for ii=1:n
@@ -52,6 +54,10 @@ ylabel('vars(spike times) / mean(spike heights)');
 xlim([0.5 2.5]);
 xticks([1 2]);
 xticklabels({'Start', 'End'});
+
+mice = mice_(idxs);
+ages = cellfun(@getperfage,mice);
+disp(ages)
 end
 
 function dat = get_groups_spikes(twdb, groups, period, eng, useFirstTask)
@@ -64,6 +70,7 @@ function ret = get_group_snr(twdb, group, period, eng, useFirstTask)
 n = length(group);
 snrsStart = nan(1,n);
 snrsEnd = nan(1,n);
+mice = cell(1,n);
 
 for ii=1:n
     [spikes,heights] = get_spikes_heights_twdb(twdb, group{ii}, period, eng, useFirstTask);
@@ -78,7 +85,8 @@ for ii=1:n
 
     snrsStart(ii) = (var([spikesStart{:}]) / 1000) / (mean(vertcat(heightsStart{:})));
     snrsEnd(ii) = (var([spikesEnd{:}]) / 1000) / (mean(vertcat(heightsEnd{:})));
+    mice{ii} = group{ii};
 end
 
-ret = [snrsStart' snrsEnd'];
+ret = {[snrsStart' snrsEnd'], mice};
 end
